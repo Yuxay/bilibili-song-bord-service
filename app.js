fea35235c6c38437aa72c687c46dcefd2cb92d30
white-yang <yangxiangyang@ccharger.com.cn>
2021-09-14 14:00:29 +0800

    feat: :sparkles: 跨域处理

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const Koa = require('koa');
 const https = require('https');
 const sslify = require('koa-sslify').default;
 const fs = require('fs');
+const cors = require('./libs/koa-cors'); //跨域处理文件koa-cors.js
 
 const app = new Koa();
+
+// app.use(cors());
+app.use(cors);
 const bodyParser = require('koa-bodyparser');
-const cors = require('koa-cors');
+// const cors = require('koa-cors');
 
 // app.use(async (ctx, next) => {
 //   ctx.set('Access-Control-Allow-Origin', '*');
@@ -50,7 +54,6 @@ app.use(async (ctx, next) => {
 
 app.on('error', (err, ctx) => console.error('server error', err));
 
-app.use(cors());
 app.use(sslify()); // 使用ssl
 
 // 路径为证书放置的位置
