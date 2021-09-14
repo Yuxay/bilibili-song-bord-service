4368f5188989a57778cfe0434150ce3dfb84a762
white-yang <yangxiangyang@ccharger.com.cn>
2021-09-14 10:48:20 +0800

    fix: :bug: 跨域配置更改

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const fs = require('fs');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 const cors = require('koa2-cors');
-const cors = require('koa2-cors');
 
 app.use(
   cors({
@@ -51,8 +50,6 @@ app.use(async (ctx, next) => {
 });
 
 app.on('error', (err, ctx) => console.error('server error', err));
-
-app.use(cors());
 
 app.use(sslify()); // 使用ssl
 
