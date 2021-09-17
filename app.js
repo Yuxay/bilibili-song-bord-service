ae3c3bb038213608deeae264a3f988e6cf68b726
white-yang <yangxiangyang@ccharger.com.cn>
2021-09-17 17:44:01 +0800

    fix: :bug: 跨域

--- app.js
+++ app.js
@@ -10,7 +10,24 @@ const fs = require('fs');
 
 const bodyParser = require('koa-bodyparser');
 const cors = require('koa2-cors');
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: function (ctx) {
+      //设置允许来自指定域名请求
+      return '*';
+      // if (ctx.url === '/test') {
+      //   return '*'; // 允许来自所有域名请求
+      // }
+      // return 'http://localhost:8080'; //只允许http://localhost:8080这个域名的请求
+    },
+    maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+    credentials: true, //是否允许发送Cookie
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], //设置获取其他自定义字段
+  })
+);
 
 const toHump = require('./utils/toHump');
 app.use(toHump); // 需要放在引用路由之前
@@ -19,13 +36,12 @@ app.use(bodyParser());
 
 app.use(router.routes());
 
-
-app.use(async (ctx, next)=> {
+app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*');
   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   if (ctx.method == 'OPTIONS') {
-    ctx.body = 200; 
+    ctx.body = 200;
   } else {
     await next();
   }
