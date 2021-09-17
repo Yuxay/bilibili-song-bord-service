c131a64ea3a1771c8f0ca626225de8dd2d9f8bb9
white-yang <yangxiangyang@ccharger.com.cn>
2021-09-17 17:41:45 +0800

    fix: :bug: 反复处理跨域

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ app.use(bodyParser());
 
 app.use(router.routes());
 
+
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200; 
+  } else {
+    await next();
+  }
+});
+
 //错误处理中间件
 app.use(async (ctx, next) => {
   try {
